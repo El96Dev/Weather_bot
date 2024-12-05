8ccaa887010702302442893fc1dca5f19e2325b0
El96Dev <el96cpp@gmail.com>
2024-12-05 12:39:12 +0300

    Added ReplyKeyboardBuilder objects instead of ReplyKeyboardMarkups, added function to create keyboard from list of cities

--- bot/keyboards/reply.py
+++ bot/keyboards/reply.py
@@ -1,25 +1,31 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove 
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 
-start_keyboard = ReplyKeyboardMarkup(
-    keyboard = [
-        [
-            KeyboardButton(text="Текущая погода"),
-            KeyboardButton(text="Прогноз погоды"),
-            KeyboardButton(text="Загрязнение воздуха"),
-            KeyboardButton(text="Добавить оповещения"),
-            KeyboardButton(text="Мои оповещения")
-        ],
-    ],
+start_keyboard = ReplyKeyboardBuilder()
+start_keyboard.add(
+    KeyboardButton(text="Текущая погода"),
+    KeyboardButton(text="Прогноз погоды"),
+    KeyboardButton(text="Загрязнение воздуха"),
+    KeyboardButton(text="Добавить оповещения"),
+    KeyboardButton(text="Мои оповещения")
 )
+start_keyboard.adjust(1, 1, 1, 1, 1)
 
-location_type_keyboard = ReplyKeyboardMarkup(
-    [
-        [
-            KeyboardButton(text="Город"),
-            KeyboardButton(text="Долгота и широта")
-        ],
-    ],
+
+location_type_keyboard = ReplyKeyboardBuilder()
+location_type_keyboard.add(
+    KeyboardButton(text="Название города"),
+    KeyboardButton(text="Долгота и широта")
 )
+location_type_keyboard.adjust(1, 1)
+
 
 remove_keyboard = ReplyKeyboardRemove()
+
+def keybord_from_cities(cities: list[dict]) -> ReplyKeyboardMarkup:
+    cities_keyboard = ReplyKeyboardBuilder()
+    for city in cities:
+        cities_keyboard.add(KeyboardButton(text=f"{city['name']}, {city['country']}, {city['state']}"))
+    cities_keyboard.adjust(1)
+    return cities_keyboard.as_markup()
